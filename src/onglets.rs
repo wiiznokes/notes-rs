70152206ecc4d5cf37bab4215b2b672ea967247b
Wiiznokes <wiiznokes@gmail.com>
2023-04-21 22:04:36 +0200

    onglets

--- src/onglets.rs
+++ src/onglets.rs
@@ -1,7 +1,7 @@
 use iced::Length;
 
 
-use crate::theme::widget::{Element, Column, Row, Text};
+use crate::theme::widget::{Element, Column, Row, Text, Container};
 
 use crate::app::{self};
 
@@ -9,6 +9,7 @@ use crate::theme:: {self};
 
 use iced::widget::{Space};
 
+use iced::alignment;
 
 
 
@@ -43,8 +44,24 @@ impl Onglets {
     pub fn view(&self) -> Element<app::Message, iced::Renderer<theme::Theme>> {
 
 
-        Text::new("hello").into()
+        let onglets = Text::new("hello")
+            .height(Length::Fill)
+            .width(Length::Fill)
+            .horizontal_alignment(alignment::Horizontal::Center)
+            .vertical_alignment(alignment::Vertical::Center);
        
+
+
+        let content = Container::new(onglets)
+            .style(theme::Container::Bordered)
+            .height(Length::Fill)
+            .width(Length::Fill);
+
+        Container::new(content)
+            .padding(10)
+            .height(Length::Fill)
+            .width(Length::Fill)
+            .into()
         
     }
 }
